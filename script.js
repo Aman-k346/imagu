5ce01b5b934dd7897cc0361b8176cd45adad57b5
Aman-k346 <amankumarg071@gmail.com>
2022-09-15 06:27:10 +0530

    initial commit

--- script.js
+++ script.js
@@ -10,33 +10,32 @@ let grayscale = document.querySelector("#grayscale");
 let invert = document.querySelector("#invert");
 let sepai = document.querySelector("#sepai");
 
-function blured() {
-    let vale = "backdrop-filter: blur(" + blur.value + "px);";
-    background.style = vale;
+function filter() {
+  let vale = "backdrop-filter: blur(" + blur.value + "px) brightness(" + brightness.value + "%) saturate(" + saturate.value + "%) hue-rotate(" + hue.value + "deg) contrast(" + contrast.value + "%)  invert(" + invert.value + "%);";
+  background.style = vale;
 }
-function saturated() {
-    let vale = "backdrop-filter: brightness(" + saturate.value + "%);";
-    background.style = vale;
-}
-function bright(){
-    let vale = "backdrop-filter: saturate(" + brightness.value + "%);";
-    background.style = vale;
-}
-function hued(){
-    let vale = "backdrop-filter: hue-rotate(" + hue.value + "deg);";
-    background.style = vale;
-}
+// function saturated() {
+//   let vale = "backdrop-filter: saturate(" + brightness.value + "%);";
+//   background.style = vale;
+// }
+// function bright() {
+//   let vale = "backdrop-filter: brightness(" + saturate.value + "%);";
+//   background.style = vale;
+// }
+// function hued() {
+//   let vale = "backdrop-filter: hue-rotate(" + hue.value + "deg);";
+//   background.style = vale;
+// }
 
-function contrasted(){
-    let vale = "backdrop-filter: contrast(" + contrast.value + "%);";
-    background.style = vale;
-}
-function grayed(){
-    let vale = "backdrop-filter: grayscale(" + grayscale.value + "%);";
-    background.style = vale;
-}
-function inverted(){
-    let vale = "backdrop-filter: invert(" + invert.value + "%);";
-    background.style = vale;
-}
-
+// function contrasted() {
+//   let vale = "backdrop-filter: contrast(" + contrast.value + "%);";
+//   background.style = vale;
+// }
+// function grayed() {
+//   let vale = "backdrop-filter: grayscale(" + grayscale.value + "%);";
+//   background.style = vale;
+// }
+// function inverted() {
+//   let vale = "backdrop-filter:;";
+//   background.style = vale;
+// }
